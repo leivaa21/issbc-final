0987a5e1e904942e4e49e0cc2dc89429bd5295f4
leivaa21 <adrianleiva212@gmail.com>
2022-05-27 14:26:07 +0200

    feat: added fixed SaveOne, added getOne and removeOne

--- src/controller/storage.py
+++ src/controller/storage.py
@@ -3,7 +3,30 @@ class StorageController:
         self.storedCases = []
 
     def saveOne(self, case):
+        index = self.__get_index(case.identificator())
+        if index != -1:
+            print('Duplicated identificator, not saved!')
+            return
         self.storedCases.append(case)
 
     def getCases(self):
         return self.storedCases
+
+    def getOne(self, identificator):
+        index = self.__get_index(identificator)
+        if index == -1:
+            return
+        return self.storedCases[index]
+
+    def removeCase(self, identificator):
+        index = self.__get_index(identificator)
+        if index == -1:
+            return
+        self.storedCases.pop(index)
+
+    def __get_index(self, identificator):
+        i = 0
+        for i in range(self.storedCases.__len__()):
+            if self.storedCases[i].identificator() == identificator:
+                return i
+        return -1
